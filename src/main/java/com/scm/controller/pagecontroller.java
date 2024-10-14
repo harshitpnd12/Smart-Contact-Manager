a06937a2321baaecbf573fd1ea8a3c0baf5c75f2
harshitpnd12 <harshit200212pandey@gmail.com>
2024-10-14 18:13:10 +0530

    solve issues

--- src/main/java/com/scm/controller/pagecontroller.java
+++ src/main/java/com/scm/controller/pagecontroller.java
@@ -3,6 +3,7 @@ package com.scm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class pagecontroller {
@@ -44,5 +45,10 @@ public class pagecontroller {
   public String register() {
     System.out.println("Register page loading");
     return "register";
+  }
+
+  @RequestMapping(value = "do-register", method = RequestMethod.POST)
+  public String processRegister() {
+    return "redirect:/register";
   }
 }
