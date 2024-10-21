e20bc75f67ce9e3820a26cd2a500c131cca1b29b
harshitpnd12 <harshit200212pandey@gmail.com>
2024-10-21 19:24:45 +0530

    work on login

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,31 +1,33 @@
 package com.scm.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import lombok.var;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
 @Configuration
 public class SecurityConfig {
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user1 = User
-        .withUsername("admin123")
-        .password("admin123")
-        .roles("ADMIN", "USER")
-        .build();
-
-    UserDetails user2 = User
-        .withUsername("Harshit")
-        .password("Harshit")
-        // .roles("ADMIN", "USER")
-        .build();
-    var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1, user2);
-    return inMemoryUserDetailsManager;
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    return daoAuthenticationProvider;
   }
+  // @Bean
+  // public UserDetailsService userDetailsService() {
+  // UserDetails user1 = User
+  // .withDefaultPasswordEncoder()
+  // .username("admin123")
+  // .password("admin123")
+  // .roles("ADMIN", "USER")
+  // .build();
+
+  // UserDetails user2 = User
+  // .withDefaultPasswordEncoder()
+  // .username("Harshit")+
+  // .password("Harshit")
+  // // .roles("ADMIN", "USER")
+  // .build();
+  // var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1,
+  // user2);
+  // return inMemoryUserDetailsManager;
+  // }
 }
