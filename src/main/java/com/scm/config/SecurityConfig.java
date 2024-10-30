9de65385e4332592099845e610457c15ed52d40c
harshitpnd12 <harshit200212pandey@gmail.com>
2024-10-30 14:07:47 +0530

    completed login page

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.scm.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
 
@@ -19,9 +21,31 @@ public class SecurityConfig {
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
     DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-    // daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    daoAuthenticationProvider.setUserDetailsService(userDetailService);
     daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
     return daoAuthenticationProvider;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    // configuration
+    httpSecurity.authorizeHttpRequests(authorize -> {
+      // authorize.requestMatchers("/home", "/register", "/login").permitAll();
+      authorize.requestMatchers("/user/**").authenticated();
+      authorize.anyRequest().permitAll();
+    });
+
+    httpSecurity.formLogin(formLogin -> {
+      formLogin.loginPage("/login");
+      formLogin.loginProcessingUrl("/authenticate");
+      formLogin.successForwardUrl("/user/dashboard");
+      formLogin.failureForwardUrl("/login?error=true");
+      formLogin.usernameParameter("email");
+      formLogin.passwordParameter("password");
+
+    });
+
+    return httpSecurity.build();
   }
 
   @Bean
@@ -29,23 +53,4 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
-  // @Bean
-  // public UserDetailsService userDetailsService() {
-  // UserDetails user1 = User
-  // .withDefaultPasswordEncoder()
-  // .username("admin123")
-  // .password("admin123")
-  // .roles("ADMIN", "USER")
-  // .build();
-
-  // UserDetails user2 = User
-  // .withDefaultPasswordEncoder()
-  // .username("Harshit")+
-  // .password("Harshit")
-  // // .roles("ADMIN", "USER")
-  // .build();
-  // var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1,
-  // user2);
-  // return inMemoryUserDetailsManager;
-  // }
 }
